69744d8bd8a4a726874a4e5b917c7aa9e27d4e9a
hungpd90 <eme2965@gmail.com>
2023-02-03 13:40:29 +0700

    Add manufacturers,products model

--- products/serializers.go
+++ products/serializers.go
@@ -1,1 +1,10 @@
 package products
+
+import "github.com/gin-gonic/gin"
+
+type ProductSerializer struct {
+	C *gin.Context
+	ProductModel
+}
+
+type ProductRe
--- products/validators.go
+++ products/validators.go
@@ -1,1 +1,50 @@
 package products
+
+import (
+	"tmdt-backend/common"
+
+	"github.com/gin-gonic/gin"
+)
+
+type CreateProductValidator struct {
+	Product struct {
+		SKU         string `form:"sku" json:"sku" binding:"requireed"`
+		Name        string `form:"name" json:"name" binding:"required"`
+		Description string `form:"description" json:"description" binding:"required"`
+		Images      string `form:"image" json:"image" binding:"required"`
+	} `json:"product"`
+	productModel ProductModel `json:"-"`
+}
+
+func NewCreateProductValidator() CreateProductValidator {
+	createProductValidator := CreateProductValidator{}
+	return createProductValidator
+}
+
+func (self *CreateProductValidator) Bind(c *gin.Context) error {
+	err := common.Bind(c, self)
+	if err != nil {
+		return err
+	}
+	self.productModel.SKU = self.Product.SKU
+	self.productModel.Name = self.Product.Name
+	self.productModel.Description = self.Product.Description
+
+	if self.Product.Images != "" {
+		self.productModel.Images = &self.Product.Images
+	}
+	return nil
+}
+
+func NewCreateProductValidatorFillWith(productModel ProductModel) CreateProductValidator {
+	createProductValidator := NewCreateProductValidator()
+	createProductValidator.Product.SKU = productModel.SKU
+	createProductValidator.Product.Name = productModel.Name
+	createProductValidator.Product.Description = productModel.Description
+
+	if productModel.Images != nil {
+		createProductValidator.Product.Images = *productModel.Images
+	}
+
+	return createProductValidator
+}
