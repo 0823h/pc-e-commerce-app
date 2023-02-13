c893cc78ec776b8d9d46a3a4a3007c60f8dd5560
hungpd90 <eme2965@gmail.com>
2023-02-13 16:31:04 +0700

    Initial commit

--- frontend/ecommerce-website/src/components/ProductList.js
+++ frontend/ecommerce-website/src/components/ProductList.js
@@ -1,19 +1,56 @@
-import React, { useEffect, useState } from "react";
+// import React from 'react';
 
-import axios from "axios";
+// import axios from 'axios';
 
-const ProductList = (props) => {
-  // eslint-disable-next-line
-  const [productList, setProductList] = useState(null);
-  axios
-    .get(`localhost:8080/api/products`)
-    .then((result) => {
-      this.setProductList(result.data);
-      console.log("abclog:" + result.data);
-    })
-    .catch((error) => console.log(error));
-  console.log("hello world");
-  return <div>Hello: {productList} HelloHello</div>;
-};
+// export default class ProductList extends React.Component {
+//   state = {
+//     persons: []
+//   }
 
-export default ProductList;
+//   componentDidMount() {
+//     axios.get(`http://localhost:8080/api/products`, {
+//       headers: {"Access-Control-Allow-Origin": "*"},
+//       responseType: "json"
+//     })
+//       .then(res => {
+//         const persons = res.data;
+//         this.setState({ persons });
+//       })
+//       .catch(error => console.log(error));
+//   }
+
+//   render() {
+//     return (
+//       <ul>
+//         { this.state.persons.map(person => <li>{person.name}</li>)}
+//       </ul>
+//     )
+//   }
+// }
+
+import React from 'react';
+
+import axios from 'axios';
+
+export default class ProductList extends React.Component {
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:8080/api/products`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <ul>
+        { this.state.persons.map(person => <li>{person.name}</li>)}
+      </ul>
+    )
+  }
+}
