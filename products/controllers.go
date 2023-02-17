8e9a1c5797bdc6c9f47066f7ac0f90b750e83b80
hungpd90 <eme2965@gmail.com>
2023-02-17 08:11:21 +0700

    Initial commit

--- products/controllers.go
+++ products/controllers.go
@@ -1,7 +1,12 @@
 package products
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"log"
 	"net/http"
+	"strings"
 	"tmdt-backend/common"
 
 	"github.com/gin-gonic/gin"
@@ -46,4 +51,98 @@ func CreateProduct(c *gin.Context) {
 
 func UpdateProduct(c *gin.Context) {
 
+}
+
+type Query struct {
+	Metadata struct {
+		page    string
+		limit   string
+		order   string
+		sort    string
+		keyword string
+	}
+	SearchKeyword struct {
+		name string
+	}
+}
+
+func QueryBind(c *gin.Context) Query {
+	page := c.Query("page")
+	if page == "" {
+		page = "0"
+	}
+	//p.Metadata.Page, _ = strconv.Atoi(page)
+
+	limit := c.Query("limit")
+	if limit == "" {
+		limit = "0"
+	}
+	//p.Metadata.Limit, _ = strconv.Atoi(limit)
+
+	order := strings.ToLower(c.Query("order"))
+	if order == "" {
+		order = "desc"
+	}
+
+	sort := strings.ToLower(c.Query("sort"))
+	if sort == "" {
+		sort = "id"
+	}
+
+	name := strings.ToLower(c.Query("name"))
+
+	var query Query
+	query.Metadata.page = page
+	query.Metadata.limit = limit
+	query.Metadata.order = order
+	query.Metadata.sort = sort
+	query.SearchKeyword.name = name
+
+	return query
+}
+
+func GetAllProductsES(c *gin.Context) {
+	es := common.GetES()
+	query := QueryBind(c)
+
+	var buf bytes.Buffer
+	var r map[string]interface{}
+
+	es_query := map[string]interface{}{
+		"query": map[string]interface{}{
+			"match": map[string]interface{}{
+				"name": query.SearchKeyword.name,
+			},
+		},
+	}
+	if err := json.NewEncoder(&buf).Encode(es_query); err != nil {
+		log.Fatalf("Error encoding query: %s", err)
+	}
+	res, err := es.Search(
+		es.Search.WithContext(context.Background()),
+		es.Search.WithIndex("product"),
+		es.Search.WithBody(&buf),
+		es.Search.WithTrackTotalHits(true),
+		es.Search.WithPretty(),
+	)
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+	}
+	defer res.Body.Close()
+
+	log.Printf(
+		"[%s] %d hits; took: %dms",
+		res.Status(),
+		int(r["hits"].(map[string]interface{})["total"].(map[string]interface{})["value"].(float64)),
+		int(r["took"].(float64)),
+	)
+
+	for _, hit := range r["hits"].(map[string]interface{})["hits"].(map[string]interface{}) {
+		log.Printf(" * ID=%s, %s", hit.(map[string]interface{})["_id"], hit.(map[string]interface{})["_source"])
+	}
+
+	log.Println(strings.Repeat("=", 37))
+
+	common.SendResponse(c, http.StatusUnprocessableEntity, err.Error(), nil)
+	return
 }
--- products/routers.go
+++ products/routers.go
@@ -5,4 +5,6 @@ import "github.com/gin-gonic/gin"
 func ProductRouter(router *gin.RouterGroup) {
 	router.GET("/", GetAllProducts)
 	router.POST("/", CreateProduct)
+	//ElasticSearch
+	router.GET("/es", GetAllProductsES)
 }
